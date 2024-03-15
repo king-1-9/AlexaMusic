82e21c108e6f88675d6afd4b12029ba9b1ff082f
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-03-16 01:04:41 +0300

    Update help.py

--- AlexaMusic/plugins/bot/help.py
+++ AlexaMusic/plugins/bot/help.py
@@ -17,7 +17,7 @@ HELP_COMMAND = get_command("HELP_COMMAND")
 
 
 @app.on_message(
-    filters.command(HELP_COMMAND) & filters.private & ~filters.edited & ~BANNED_USERS
+    filters.command(HELP_COMMAND) & filters.private & ~BANNED_USERS
 )
 @app.on_callback_query(filters.regex("settings_back_helper") & ~BANNED_USERS)
 async def helper_private(
@@ -52,7 +52,7 @@ async def helper_private(
 
 
 @app.on_message(
-    filters.command(HELP_COMMAND) & filters.group & ~filters.edited & ~BANNED_USERS
+    filters.command(HELP_COMMAND) & filters.group & ~BANNED_USERS
 )
 @LanguageStart
 async def help_com_group(client, message: Message, _):
