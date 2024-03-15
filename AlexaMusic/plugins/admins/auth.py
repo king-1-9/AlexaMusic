9ae1b382cf2247366992aeb704d5ce3d2f632e13
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-03-16 00:55:38 +0300

    Update auth.py

--- AlexaMusic/plugins/admins/auth.py
+++ AlexaMusic/plugins/admins/auth.py
@@ -31,7 +31,7 @@ AUTHUSERS_COMMAND = get_command("AUTHUSERS_COMMAND")
 
 
 @app.on_message(
-    filters.command(AUTH_COMMAND) & filters.group & ~filters.edited & ~BANNED_USERS
+    filters.command(AUTH_COMMAND) & filters.group & ~BANNED_USERS
 )
 @AdminActual
 async def auth(client, message: Message, _):
@@ -95,7 +95,7 @@ async def auth(client, message: Message, _):
 
 
 @app.on_message(
-    filters.command(UNAUTH_COMMAND) & filters.group & ~filters.edited & ~BANNED_USERS
+    filters.command(UNAUTH_COMMAND) & filters.group & ~BANNED_USERS
 )
 @AdminActual
 async def unauthusers(client, message: Message, _):
@@ -130,7 +130,7 @@ async def unauthusers(client, message: Message, _):
 
 
 @app.on_message(
-    filters.command(AUTHUSERS_COMMAND) & filters.group & ~filters.edited & ~BANNED_USERS
+    filters.command(AUTHUSERS_COMMAND) & filters.group & ~BANNED_USERS
 )
 @language
 async def authusers(client, message: Message, _):
