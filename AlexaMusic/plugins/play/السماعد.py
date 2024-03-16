5d64759d75fbfde7b7ceafcb2fa66de301101017
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-03-16 06:29:20 +0300

    Update السماعد.py

--- AlexaMusic/plugins/play/السماعد.py
+++ AlexaMusic/plugins/play/السماعد.py
@@ -21,8 +21,8 @@ from pyrogram import filters
 
 load_dotenv()
 
-ass_us = config.ASS_US
-ass_id = config.ASS_ID
+ASS_US = getenv("ASS_US")
+ASS_ID = getenv("ASS_ID")
 
 def get_file_id(msg: Message):
     if msg.media:
@@ -49,24 +49,25 @@ def get_file_id(msg: Message):
 @app.on_message(
     command(["انضم","الحساب المساعد","حساب مساعد"])
     & filters.group
+    & ~filters.edited
 )
 async def khalid(client: Client, message: Message):
-    usr = await client.get_users(ass_id)
+    usr = await client.get_users(ASS_ID)
     name = usr.first_name
-    async for photo in client.iter_profile_photos(ass_id, limit=1):
-        await message.reply_photo(photo.file_id, caption=f"""❲ معلومات الحساب المساعد ❳
+    async for photo in client.iter_profile_photos(ASS_ID, limit=1):
+                    await message.reply_photo(photo.file_id,       caption=f"""❲ معلومات الحساب المساعد ❳
 — — — — — — — — —
-- اسم الحساب المساعد : [{usr.first_name}](https://t.me/{ass_us})
-- يوزر الحساب المساعد : @{ass_us}""", 
-            reply_markup=InlineKeyboardMarkup(
+- اسم الحساب المساعد : [{usr.first_name}](https://t.me/{ASS_US})
+- يوزر الحساب المساعد : @{ASS_US}""", 
+        reply_markup=InlineKeyboardMarkup(
+            [
                 [
-                    [
-                        InlineKeyboardButton(
-                            name, url=f"tg://user?id={ass_id}")
-                    ],[
-                        InlineKeyboardButton(
-                            "࿈ ضيف الحساب المساعد لمجموعتك .", url=f"https://t.me/{ass_us}?startgroup=true"),
-                    ],
-                ]
-            ),
-        )
+                    InlineKeyboardButton(
+                        name, url=f"tg://user?id={ASS_ID}")
+                ],[
+                    InlineKeyboardButton(
+                        "࿈ ضيف الحساب المساعد لمجموعتك .", url=f"https://t.me/{ASS_US}?startgroup=true"),
+                ],
+            ]
+        ),
+    )
