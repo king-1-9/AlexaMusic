fde77d383dd53865eff82ff3ad3e496027313764
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-03-16 05:52:48 +0300

    Update userbot.py

--- AlexaMusic/core/userbot.py
+++ AlexaMusic/core/userbot.py
@@ -36,12 +36,7 @@ class Userbot(Client):
         self.username = get_me.username
         self.id = get_me.id
         self.mention = get_me.mention
-        try:
-            await self.join_chat("EF_19")
-            await self.join_chat("jnssghb")
-        except:
-            pass
-            assistants.append(1)
+        assistants.append(1)
         try:
             await self.send_message(
                 config.LOG_GROUP_ID, "『 تم تشغيل البوت على سورس الملك 』"
